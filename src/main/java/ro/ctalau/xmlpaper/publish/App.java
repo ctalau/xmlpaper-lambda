57b64fa83f401112b97c6cff3d0fca383f27dcf3
ctalau <cristitalau@gmail.com>
2019-06-05 00:19:31 +0300

    Run Docbook xsl in stage 2

--- src/main/java/ro/ctalau/xmlpaper/publish/App.java
+++ src/main/java/ro/ctalau/xmlpaper/publish/App.java
@@ -1,5 +1,6 @@
 package ro.ctalau.xmlpaper.publish;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
@@ -11,7 +12,6 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.xml.transform.Result;
 import javax.xml.transform.Source;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
@@ -28,23 +28,49 @@ public class App implements RequestStreamHandler {
 
   public void handleRequest(InputStream input, OutputStream output, Context arg2) throws IOException {
     // Everything is unpacked in "/var/task".
-    File xslFile = new File("/var/task/resources/addPlaceholders.xsl");
-    Source xsl = new StreamSource(xslFile);
+    String resourcesFolder = "/var/task/resources/";
+    File xslFile = new File(resourcesFolder + "addPlaceholders.xsl");
+    File xslFile2 = new File(resourcesFolder + "docbook/xsl/fo/docbook_custom.xsl");
+    
     
     // The input is a JSON object with a body field. 
     ObjectMapper mapper = new ObjectMapper();
     JsonNode event = mapper.readTree(input);
     String payload = event.get("body").asText();
-    System.out.println(payload);
     Source xmlInput = new StreamSource(new StringReader(payload));
     
-    
-    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-    Result result = new StreamResult(outputStream);
+    ByteArrayOutputStream buffer2 = new ByteArrayOutputStream();
 
     try {
-      Transformer transformer = new com.icl.saxon.TransformerFactoryImpl().newTransformer(xsl);
-      transformer.transform(xmlInput, result);
+      ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+      
+      com.icl.saxon.TransformerFactoryImpl factoryImpl = new com.icl.saxon.TransformerFactoryImpl();
+      Transformer transformer = factoryImpl.newTransformer(new StreamSource(xslFile));
+      transformer.transform(xmlInput, new StreamResult(buffer));
+      
+      Transformer transformer2 = factoryImpl.newTransformer(new StreamSource(xslFile2));
+      transformer2.setParameter("admon.graphics", 1);
+      transformer2.setParameter("admon.graphics.extension", ".png");
+      transformer2.setParameter("admon.graphics.path", 
+          resourcesFolder + "docbook/css/img/");
+      transformer2.setParameter("body.font.family", "Times New Roman, Arial Unicode MS, Tahoma, Batang, serif");
+      transformer2.setParameter("callout.graphics.path", 
+          resourcesFolder + "docbook/xsl/images/callouts/");
+      transformer2.setParameter("draft.mode", "no");
+      transformer2.setParameter("fop.extensions", 0);
+      transformer2.setParameter("fop1.extensions", 0);
+      transformer2.setParameter("highlight.source", 1);
+      transformer2.setParameter("highlight.xslthl.config", 
+          "file://" + resourcesFolder + "docbook/xsl/highlighting/xslthl-config.xml");
+      transformer2.setParameter("admon.graphics", 1);
+      transformer2.setParameter("monospace.font.family", 
+          "monospace, Courier New, Courier, Consolas, Liberation Mono, Arial Unicode MS");
+      transformer2.setParameter("paper.type", "A4");
+      transformer2.setParameter("title.font.family", 
+          "Arial, Arial Unicode MS, Tahoma, Batang, sans-serif");
+      transformer2.transform(
+          new StreamSource(new ByteArrayInputStream(buffer.toByteArray())), 
+          new StreamResult(buffer2));
     } catch (TransformerException e) {
       throw new IOException("Cannot transform", e);
     }
@@ -53,7 +79,7 @@ public class App implements RequestStreamHandler {
     Map<String, Object> response = new HashMap<>();
     response.put("headers", Collections.emptyMap());
     response.put("statusCode", 200);
-    String transformed = new String(outputStream.toByteArray(), StandardCharsets.UTF_8);
+    String transformed = new String(buffer2.toByteArray(), StandardCharsets.UTF_8);
     System.out.println(transformed);
     response.put("body", transformed);
     
